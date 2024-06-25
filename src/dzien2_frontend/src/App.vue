83dc874bd0ba6b7647a64516c9f47488fcf28ed7
pu0238 <pu0238@protonmail.com>
2024-06-25 19:27:59 +0200

    added blog component

--- src/dzien2_frontend/src/App.vue
+++ src/dzien2_frontend/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 import { dzien2_backend } from 'declarations/dzien2_backend/index';
+import Blog from './components/Blog.vue';
+
 let greeting = ref('');
 
 async function handleSubmit(e) {
@@ -26,5 +28,6 @@ async function handleSubmit(e) {
       <button type="submit">Click Me!</button>
     </form>
     <section id="greeting">{{ greeting }}</section>
+    <Blog />
   </main>
 </template>
---
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        siema blog!
+    </div>
+</template>

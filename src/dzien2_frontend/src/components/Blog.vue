e3061a6a05c84a84abddb54c20aacec11f215b63
pu0238 <pu0238@protonmail.com>
2024-06-25 19:56:22 +0200

    Added a way to add new wpis

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -3,6 +3,8 @@
         <button @click="pobierzWpisy">refresh</button>
         siema blog!
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
     </div>
 </template>
 
@@ -12,10 +14,14 @@ import { dzien2_backend } from 'declarations/dzien2_backend/index';
 export default {
     data() {
         return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
+        async dodajWpisy() {
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+        },
         async pobierzWpisy() {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }

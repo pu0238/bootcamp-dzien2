03691fa35fdfdf678d69d50e3c8291befa797bd6
pu0238 <pu0238@protonmail.com>
2024-06-25 18:57:56 +0200

    added way to add wpisy

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,4 +1,17 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
 fn greet(name: String, last_name: i8) -> String {
     format!("Hello, {} {}!", name, last_name)
+}
+
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String) {
+    WPISY.with(|wpisy| {
+        wpisy.borrow_mut().push(wpis)
+    });
 }

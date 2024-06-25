81373891ee5942c082a5986bcfc337d074d8244d
pu0238 <pu0238@protonmail.com>
2024-06-25 19:43:00 +0200

    added way to list wpisy

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
+        <button @click="pobierzWpisy">refresh</button>
         siema blog!
+        {{ wpisy }}
     </div>
 </template>
+
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+
+export default {
+    data() {
+        return {
+            wpisy: []
+        }
+    },
+    methods: {
+        async pobierzWpisy() {
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
+        }
+    },
+
+}
+</script>

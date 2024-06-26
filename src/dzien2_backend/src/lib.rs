e292ef7b2872b9b3e7a7919c10ac6be0b06822ed
pu0238 <pu0238@protonmail.com>
2024-06-26 17:37:31 +0200

    Added usun_wpis and edytuj_wpis

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -22,3 +22,20 @@ fn odczytaj_wpisy() -> Vec<String> {
         wpisy.borrow().clone()
     })
 }
+
+#[ic_cdk::update]
+fn usun_wpis(id_wpisu: usize){
+    WPISY.with(|wpisy| {
+        wpisy.borrow_mut().remove(id_wpisu)
+    });
+}
+
+#[ic_cdk::update]
+fn edytuj_wpis(id_wpisu: usize, nowy_wpis: String){
+    WPISY.with(|wpisy| {
+        let mut binding = wpisy.borrow_mut();
+        let wpis = binding.get_mut(id_wpisu);
+        let stary_wpis = wpis.unwrap();
+        *stary_wpis = nowy_wpis;
+    });  
+}

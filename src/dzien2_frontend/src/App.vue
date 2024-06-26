8d01bcca75c0e8a27ae789508715233431d490ff
pu0238 <pu0238@protonmail.com>
2024-06-26 19:05:58 +0200

    added styles

--- src/dzien2_frontend/src/App.vue
+++ src/dzien2_frontend/src/App.vue
@@ -1,34 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import { dzien2_backend } from 'declarations/dzien2_backend/index';
 import Blog from './components/Blog.vue';
-
-let greeting = ref('');
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  const numer = target.querySelector('#numer').value;
-  await dzien2_backend.greet(name, Number(numer)).then((response) => {
-    greeting.value = response;
-  });
-}
 </script>
 
 <template>
-  <main>
+  <main class="container mx-auto">
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <h1 class="text-blue-600">Siema?!</h1>
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <input id="numer" alt="Numer" type="number" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
     <Blog />
   </main>
 </template>
--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        siema blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+            <button  class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
+        </div>
     </div>
 </template>
 
@@ -26,6 +34,8 @@ export default {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
-
+    async mounted(){
+        this.pobierzWpisy()
+    }
 }
 </script>
